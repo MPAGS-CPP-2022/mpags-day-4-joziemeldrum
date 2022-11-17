532906bde12a5917f78f47398345ddf4ad34533f
Jozie Cottee Meldrum <sd22284@bristol.ac.uk>
2022-11-17 13:22:04 +0000

    finished encryption

--- src/MPAGSCipher/PlayfairCipher.cpp
+++ src/MPAGSCipher/PlayfairCipher.cpp
@@ -55,6 +55,8 @@ void PlayfairCipher::setKey(const std::string& key)
 
     auto rem_duplicates_iter = std::remove_if(key_.begin(), key_.end(), isduplicate);
     key_.erase(rem_duplicates_iter, key_.end());
+
+    std::cout << key_ << std::endl;
   
     // Store the coords of each letter
 
@@ -78,33 +80,105 @@ void PlayfairCipher::setKey(const std::string& key)
         letter2coord[letter] = coord;
         coord2letter[coord] = letter;
     }
-    
-    // Store the playfair cipher key map
-    for (auto p : letter2coord){
-        std::cout << p.first << " : {" << p.second.first << " , " << p.second.second << " }" << std::endl;
-    }
 
+    // Store maps for later
+    letterToPosition_ = letter2coord;
+    positionToLetter_ = coord2letter;
+    
 }
 
-std::string PlayfairCipher::applyCipher(const std::string& inputText, const CipherMode cipherMode ) const
+std::string PlayfairCipher::applyCipher(const std::string& inputText /*, const CipherMode cipherMode*/ ) const
 {
-// Change J → I
+    size_t text_length{inputText.size()};
+
+    std::string outputText{""};
+
+    using CharPair_t = std::pair<char, char>;
+    CharPair_t digraph{'x', 'x'};
+
+    using SizePair_t = std::pair<size_t, size_t>; // row then column coord
+    SizePair_t coordinates[2] = {{0, 0}, {0, 0}};
+
+    size_t temp_storage{0}; // temporary storage for when shuffling coordinates later
+
+    // Change J → I
+    for (size_t i{0} ; i < text_length ; i++){
+        //make a digraph
+        // check for J:
+        if (inputText[i] == 'J'){
+            digraph.first = 'I';
+        }
+        else{
+            digraph.first = inputText[i];
+        }
+        // second part of digraph could be I, X, Z, or the next letter:
+        // 1. check for J:
+        if (inputText[i+1] == 'J'){
+            digraph.second = 'I';
+            i++;
+        }
+        // 2. check for repeated letter:
+        else if (inputText[i+1] == inputText[i]){
+            digraph.second = 'X';
+        }
+        // 3. check for last character in string
+        else if ( i+1 == text_length){
+            digraph.second = 'Z';
+        }
+        // 4. otherwise just use the next character
+        else {
+            digraph.second = inputText[i+1];
+            i++;
+        }
 
 
-// If repeated chars in a digraph add an X or Q if XX
+        // Find the coordinates:
+        coordinates[0] = letterToPosition_.at(digraph.first);
+        coordinates[1] = letterToPosition_.at(digraph.second);
 
-// if the size of input is odd, add a trailing Z
+        // Option 1 - letters on the same row
+        if (coordinates[0].first == coordinates[1].first){
+            // add one to each coordinate column
+            for (int j{0}; j < 2; j++){
+                coordinates[j].second++;
 
-// Loop over the input in Digraphs
+                // check for wrapping:
+                if (coordinates[j].second > 5){
+                    coordinates[j].second %= 5;
+                }
+            }
+        }
+        // Option 2 - letters in same column
+        else if(coordinates[0].second == coordinates[1].second){
+            for (int j{0}; j < 2; j++){
+                // add one to each coordinate row
+                coordinates[j].first++;
 
-// - Find the coords in the grid for each digraph
+                // check for wrapping:
+                if (coordinates[j].first > 4){
+                    coordinates[j].first %= 5; //THINK THERE IS A PROBLEM HERE
+                }
+            }
+        }
+        // Option 3 - forms a rectangle
+        else{
+            // row of each stay the same
+            // swap column indices of each coord
+            temp_storage = coordinates[0].second;
+            coordinates[0].second = coordinates[1].second;
+            coordinates[1].second = temp_storage;         
+        }
+        
+        // Convert back to text
+        digraph.first = positionToLetter_.at(coordinates[0]);
+        digraph.second = positionToLetter_.at(coordinates[1]);
 
-// - Apply the rules to these coords to get 'new' coords
+        // Add it to the output text
+        outputText += digraph.first + digraph.second;      
+    } 
 
-// - Find the letter associated with the new coords
 
-// return the text
-return ""; 
+return outputText; 
 }
 
 std::string PlayfairCipher::getKey(){
